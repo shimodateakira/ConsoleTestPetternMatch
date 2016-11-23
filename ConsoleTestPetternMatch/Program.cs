9b329fadbb6aaeeb9cb62e06e327413f16c6d8e0
下舘朗 <shimodateakira@i.softbank.jp>
2016-11-23 20:30:02 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleTestPetternMatch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleTestPetternMatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            while((s = Console.ReadLine()) != "")
+            {
+                // C#6までの書き方
+                int n1 = 0;
+                if (int.TryParse(s, out n1))
+                {
+                    Console.WriteLine($"入力した数字は{n1}です。");
+                }
+                else
+                {
+                    Console.WriteLine($"数字ではありません");
+                }
+
+                // C#7ではパターンマッチングと拡張メソッドでこのように書ける
+                if (s.Parse() is int n2)
+                {
+                    Console.WriteLine($"入力した数字は{n2}です。");
+                }
+                else
+                {
+                    Console.WriteLine($"数字ではありません");
+                }
+            }
+        }
+    }
+
+    static class StringExtension
+    {
+        public static int? Parse(this string s)
+        {
+            int n;
+            return int.TryParse(s, out n) ? (int?)n : null;
+        }
+    }
+}
